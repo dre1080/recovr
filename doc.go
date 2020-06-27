35f6b0abdbded38255ce38d4baf3ea88e8d73840
ando <lifeandcoding@gmail.com>
2020-06-27 04:54:27 +0300

    release: v1.0.2
    * fix docs for pkg.go.dev

--- doc.go
+++ doc.go
@@ -1,22 +1,20 @@
-/*Package recovr is a HTTP middleware that catches any panics and serves a proper error response.
-
-package main
-
-import (
-	"log"
-	"net/http"
-
-	"github.com/dre1080/recovr"
-)
-
-var myPanicHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	panic("you should not have a handler that just panics ;)")
-})
-
-func main() {
-	recovery := recovr.New()
-	app := recovery(myPanicHandler)
-	http.ListenAndServe("0.0.0.0:3000", app)
-}
-*/
+// Package recovr is a HTTP middleware that catches any panics and serves a proper error response.
+//
+// 	package main
+//
+// 	import (
+// 		"log"
+// 		"net/http"
+// 		"github.com/dre1080/recovr"
+// 	)
+//
+// 	var myPanicHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		panic("you should not have a handler that just panics ;)")
+// 	})
+//
+// 	func main() {
+// 		recovery := recovr.New()
+// 		app := recovery(myPanicHandler)
+// 		http.ListenAndServe("0.0.0.0:3000", app)
+// 	}
 package recovr
